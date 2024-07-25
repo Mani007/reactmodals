099b0c2f7b9f110aaf7681141bb437fe73ae6283
Mohnish <mohnish.a007@gmail.com>
2024-07-25 19:37:36 +0100

    Working!! modal with with outside click close

--- src/components/Showmodal.jsx
+++ src/components/Showmodal.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Showmodal({toggleModal}) {
   return (
     <>
-    <div className='modal-wrapper'></div>
+    <div className='modal-wrapper' onClick={toggleModal}></div>
     <div className='modal-container'>
                 <h2>This is a modal</h2>
                 <button onClick={toggleModal}>Close</button>
