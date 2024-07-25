63b48ef9f3a429e66417ea3bddc04f6a2fcda55b
Mohnish <mohnish.a007@gmail.com>
2024-07-25 23:44:10 +0100

    MODAL Working perfectly using createPortal in ReactDOM

--- src/components/Showmodal.jsx
+++ src/components/Showmodal.jsx
@@ -1,21 +1,22 @@
 import React, { useEffect } from 'react'
-
+import ReactDOM from 'react-dom'
 function Showmodal({toggleModal}) {
     useEffect(() =>{
         document.body.style.overflowY = 'hidden';
         return () => document.body.style.overflowY = 'auto'; // when component unmounts, restore scroll a.k.a as cleanup function 
     },[])
-  return (
+  return ReactDOM.createPortal(
     <>
-    <div className='testing'>
+    {/* <div className='testing'> */}
 
     <div className='modal-wrapper' onClick={toggleModal}></div>
     <div className='modal-container'>
                 <h2>This is a modal</h2>
                 <button onClick={toggleModal}>Close</button>
     </div>
-    </div>
-    </>
+    {/* </div> */}
+    </>, 
+    document.getElementById('modal-part') // Location where in the DOM the modal is located
   )
 }
 
